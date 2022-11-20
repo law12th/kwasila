fabf344bdf79714d767bece6de9ff09318b9f7f6
law12th <lawrencetepula@gmail.com>
2022-11-20 07:58:08 +0200

    testing

---
+++ apps/auth/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+	extends: ["custom"],
+	parserOptions: {
+		root: true,
+		tsconfigRootDir: __dirname,
+		project: ["./tsconfig.json"],
+	},
+};
--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,70 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
-  },
+	env: {
+		node: true,
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"airbnb-base",
+		"airbnb-typescript/base",
+		"turbo",
+		"plugin:import/errors",
+		"plugin:import/warnings",
+		"plugin:import/typescript",
+		"prettier",
+	],
+	plugins: ["import", "@typescript-eslint"],
+	parserOptions: {
+		project: ["./tsconfig.json"],
+	},
+	settings: {
+		"import/parsers": {
+			"@typescript-eslint/parser": [".ts"],
+		},
+		"import/resolver": {
+			node: {
+				extensions: [".js", ".ts"],
+				moduleDirectory: ["node_modules", "src/"],
+			},
+			typescript: {
+				alwaysTryTypes: true,
+				project: ".",
+			},
+		},
+	},
+	overrides: [
+		{
+			env: {
+				jest: true,
+			},
+			files: ["**/__tests__/**/*.[jt]s", "**/?(*.)+(spec|test).[jt]s"],
+			extends: ["plugin:jest/recommended"],
+			rules: {
+				"import/no-extraneous-dependencies": [
+					"off",
+					{ devDependencies: ["**/?(*.)+(spec|test).[jt]s"] },
+				],
+			},
+		},
+	],
+	rules: {
+		"no-unused-vars": "error",
+		"no-unreachable": "error",
+		"no-invalid-regexp": "warn",
+		"no-self-import": "off",
+		"no-empty": "error",
+		"no-empty-function": "warn",
+		"no-magic-numbers": "warn",
+		"no-var": "error",
+		"prefer-const": "warn",
+		"no-multi-spaces": "error",
+	},
+	ignorePatterns: [
+		"**/*.js",
+		"node_modules",
+		".turbo",
+		"dist",
+		"coverage",
+		"build",
+	],
 };
