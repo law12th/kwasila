eeeddca58043e85cf0a219a48d7ffd0646ecdcc9
law12th <lawrencetepula@gmail.com>
2022-11-20 12:58:20 +0200

    refactor(logger-transports/index.ts): add eslint disable statements for temp fix

--- packages/kw-logging/src/logger-transports/index.ts
+++ packages/kw-logging/src/logger-transports/index.ts
@@ -1,4 +1,7 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable default-case */
 /* eslint-disable consistent-return */
+/* eslint-disable arrow-body-style */
 import {
 	createConsoleTransport,
 	createFileTransport,
@@ -6,15 +9,15 @@ import {
 import TransportTypes from "../transport-types";
 import { Transport } from "../transports";
 
-const getLoggerTransports = (transportsConfig: Transport[]) =>
-	transportsConfig.forEach(({ type, options }) => {
+const getLoggerTransports = (transportsConfig: Transport[]) => {
+	return transportsConfig.map(({ type, options }) => {
 		switch (type) {
 			case TransportTypes.CONSOLE:
 				return createConsoleTransport(options);
 			case TransportTypes.FILE:
 				return createFileTransport(options);
-			default:
 		}
 	});
+};
 
 export default getLoggerTransports;
